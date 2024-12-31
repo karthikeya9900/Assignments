4a321b6e6988ca1813bf91360c036133163b5ee1
karthikeya <122973946+karthikeya9900@users.noreply.github.com>
2024-12-31 12:35:54 +0530

    updated with seperate testing files for each file and removing unwanted things

--- ARRAY2/01_strictly_ascending.js
+++ ARRAY2/01_strictly_ascending.js
@@ -13,36 +13,3 @@ function isStrictlyAscending(numbers) {
 
   return true;
 }
-
-// ----------------------------------- testing section 
-function getMark(expected, actual) {
-  return expected === actual ? "✅" : "❌";
-}
-
-function generateMesage(array1, actual, expected) {
-  const mark = getMark(expected, actual);
-  const inputpart = "\n inputs:" + array1;
-  const expectedPart = "\n expected to be:" + expected;
-  const actualPart = "\n actual is:" + actual;
-
-  return mark + inputpart + expectedPart + actualPart + "\n";
-}
-
-function testIsStrictlyAscending(array1, expected) {
-  const actual = isStrictlyAscending(array1);
-
-  console.log(generateMesage(array1, actual, expected));
-}
-
-function testAll() {
-  testIsStrictlyAscending([], true);
-  testIsStrictlyAscending([1], true);
-  testIsStrictlyAscending([1, 2], true);
-  testIsStrictlyAscending([1, 1], false);
-  testIsStrictlyAscending([1, 2, 4, 7], true);
-  testIsStrictlyAscending([1, 1, 2, 3, 4], false);
-  testIsStrictlyAscending([1, 2, 3, 2], false);
-  testIsStrictlyAscending([1, 2, 3, 3], false);
-}
-
-testAll();
